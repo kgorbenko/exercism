6dd031847edef3d3ec5104cc1e710ac01858b5e9
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-08-25 23:11:22 +0300

    Add existing solution

---
+++ csharp/circular-buffer/CircularBuffer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CircularBuffer<T>
+{
+    private readonly Queue<T> queue;
+    private readonly int capacity;
+
+    public CircularBuffer(int capacity)
+    {
+        queue         = new Queue<T>(capacity);
+        this.capacity = capacity;
+    }
+
+    public T Read() => queue.Dequeue();
+
+    public void Write(T value)
+    {
+        if (queue.Count == capacity)
+        {
+            throw new InvalidOperationException("Full buffer can't be written to.");
+        }
+
+        queue.Enqueue(value);
+    }
+
+    public void Overwrite(T value)
+    {
+        if (queue.Count == capacity)
+        {
+            queue.Dequeue();
+        }
+
+        Write(value);
+    }
+
+    public void Clear() => queue.Clear();
+}
