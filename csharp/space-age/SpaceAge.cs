8a22d32b6c6db0fcce114623af5035908c479bf4
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-08-25 10:12:23 +0300

    Add iteration

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -2,7 +2,9 @@ using System;
 
 public class SpaceAge
 {
-    private long seconds;
+    private const int EarthPeriod = 31557600;
+
+    private readonly long seconds;
 
     public SpaceAge(long seconds)
     {
@@ -11,49 +13,22 @@ public class SpaceAge
 
     private double CalculateAge(double period)
     {
-        var earthPeriod = 31557600;
-
-        return Math.Round(seconds / (earthPeriod * period), 2);
+        return Math.Round(seconds / (EarthPeriod * period), digits: 2);
     }
 
+    public double OnEarth() => CalculateAge(period: 1);
 
-    public double OnEarth()
-    {
-        return CalculateAge(1);
-    }
+    public double OnMercury() => CalculateAge(period: 0.2408467);
 
-    public double OnMercury()
-    {
-        return CalculateAge(0.2408467);
-    }
+    public double OnVenus() => CalculateAge(period: 0.61519726);
 
-    public double OnVenus()
-    {
-        return CalculateAge(0.61519726);
-    }
+    public double OnMars() => CalculateAge(period: 1.8808158);
 
-    public double OnMars()
-    {
-        return CalculateAge(1.8808158);
-    }
+    public double OnJupiter() => CalculateAge(period: 11.862615);
 
-    public double OnJupiter()
-    {
-        return CalculateAge(11.862615);
-    }
+    public double OnSaturn() => CalculateAge(period: 29.447498);
 
-    public double OnSaturn()
-    {
-        return CalculateAge(29.447498);
-    }
+    public double OnUranus() => CalculateAge(period: 84.016846);
 
-    public double OnUranus()
-    {
-        return CalculateAge(84.016846);
-    }
-
-    public double OnNeptune()
-    {
-        return CalculateAge(164.79132);
-    }
+    public double OnNeptune() => CalculateAge(period: 164.79132);
 }
