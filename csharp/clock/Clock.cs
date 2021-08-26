8f707fc4fe6f10780c07ed97056fa616efadcebc
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-08-26 10:04:20 +0300

    Add iteration

--- csharp/clock/Clock.cs
+++ csharp/clock/Clock.cs
@@ -1,64 +1,27 @@
-using System;
-
 public struct Clock
 {
-    private int hours;
-    private int minutes;
+    private const int MinutesInHour = 60;
+    private const int HoursInDay = 24;
+    private const int MinutesInDay = MinutesInHour * HoursInDay;
+
+    public int Hours { get; }
+
+    public int Minutes { get; }
 
     public Clock(int hours, int minutes)
     {
-        this.hours = hours + minutes / 60;
-        while (this.hours >= 24 || this.hours < 0)
-            this.hours -= Math.Sign(this.hours)*24;
+        var totalMinutes    = minutes + hours * MinutesInHour;
+        var positiveMinutes = (totalMinutes % MinutesInDay + MinutesInDay) % MinutesInDay;
 
-        this.minutes = minutes % 60;
-        while (this.minutes < 0)
-            { this.minutes += 60; this.hours--; }
-    }
-
-    public int Hours
-    {
-        get { return hours; }
-    }
-
-    public int Minutes
-    {
-        get { return minutes; }
+        Hours   = positiveMinutes / MinutesInHour;
+        Minutes = positiveMinutes % MinutesInHour;
     }
 
     public Clock Add(int minutesToAdd)
-    {
-        if (minutesToAdd < 0) return Subtract(-minutesToAdd);
-
-        hours += (minutes + minutesToAdd) / 60;
-        minutes = (minutes + minutesToAdd) % 60;
-
-        while (hours >= 24) hours -= 24;
-        return new Clock(hours, minutes);
-    }
+        => new Clock(Hours, Minutes + minutesToAdd);
 
     public Clock Subtract(int minutesToSubtract)
-    {
-        if (minutesToSubtract < 0) return Add(-minutesToSubtract);
+        => new Clock(Hours, Minutes - minutesToSubtract);
 
-        minutesToSubtract -= minutes; minutes = 0;
-        hours -=  minutesToSubtract / 60 + 1;
-        minutes = 60 - (minutesToSubtract % 60);
-
-        while (hours < 0) hours += 24;
-        return new Clock(hours, minutes);
-    }
-
-    public override string ToString()
-    {
-        string hour = "00", minute = "00";
-
-        if (hours < 10) hour = $"0{hours.ToString()}";
-        else hour = hours.ToString();
-
-        if (minutes < 10) minute = $"0{minutes.ToString()}";
-        else minute = minutes.ToString();
-
-        return hour + ':' + minute; 
-    }
+    public override string ToString() => $"{Hours:00}:{Minutes:00}";
 }
