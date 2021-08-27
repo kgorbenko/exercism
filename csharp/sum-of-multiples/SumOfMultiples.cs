72f4ba7f1d7a0efc8dfe77b0c23c37b7b2f13246
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-08-27 11:23:51 +0300

    Add existing solution

---
+++ csharp/sum-of-multiples/SumOfMultiples.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+
+public static class SumOfMultiples
+{
+    public static int To(IEnumerable<int> multiples, int max)
+    {
+        var sum = 0;
+        var invalidNumbers = new List<int>();
+
+        foreach (var c in multiples)
+        {
+            for (var i = c; i < max; i += c)
+            {
+                if ((i % c) == 0 && i < max && !invalidNumbers.Contains(i))
+                {
+                    invalidNumbers.Add(i);
+                    sum += i;
+                }
+            }
+        }
+
+        return sum;
+    }
+}
