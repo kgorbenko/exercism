51cc5cc9f0d31abba37fcefbf4eb6a715bec01b9
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-08-27 12:20:43 +0300

    Add iteration

--- csharp/series/Series.cs
+++ csharp/series/Series.cs
@@ -1,19 +1,17 @@
 using System;
-using System.Collections.Generic;
+using System.Linq;
 
 public static class Series
 {
     public static string[] Slices(string numbers, int sliceLength)
     {
-        if (numbers.Length == 0) throw new ArgumentException("Empty input string");
-        if (sliceLength <= 0) throw new ArgumentException("Slice length can not be less then or equal to 0");
-        if (sliceLength > numbers.Length) throw new ArgumentException("Slice length can not be more than numbers string length");
+        if (sliceLength <= 0 || sliceLength > numbers.Length)
+        {
+            throw new ArgumentException();
+        }
 
-        var slices = new List<string>();
-
-        for (int i = 0; i <= numbers.Length - sliceLength; i++)
-            slices.Add(numbers.Substring(i, sliceLength));
-
-        return slices.ToArray();
+        return Enumerable.Range(start: 0, numbers.Length - sliceLength + 1)
+                         .Select(x => numbers.Substring(x, sliceLength))
+                         .ToArray();
     }
 }
