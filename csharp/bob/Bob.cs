f52fa4ceba887667f6fd975966cebd1c46645e11
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-08-27 09:53:52 +0300

    Add iteration

--- csharp/bob/Bob.cs
+++ csharp/bob/Bob.cs
@@ -1,19 +1,20 @@
-using System;
 using System.Linq;
 
 public static class Bob
 {
     public static string Response(string statement)
     {
-        if (string.IsNullOrWhiteSpace(statement))
-            return "Fine. Be that way!";
+        bool IsQuestion(string statement) => statement.EndsWith("?");
 
-        if (statement.ToUpper().Equals(statement) && statement.Any(x => char.IsLetter(x)))
-            return "Whoa, chill out!";
+        bool IsYell(string statement)
+            => statement.Any(char.IsLetter) && statement.ToUpper() == statement;
 
-        if (statement.Trim().EndsWith("?"))
-            return "Sure.";
-
-        return "Whatever.";
+        return statement.Trim() switch {
+            var str when string.IsNullOrWhiteSpace(str) => "Fine. Be that way!",
+            var str when IsYell(str) && IsQuestion(str) => "Calm down, I know what I'm doing!",
+            var str when IsQuestion(str) => "Sure.",
+            var str when IsYell(str) => "Whoa, chill out!",
+            _ => "Whatever."
+        };
     }
 }
