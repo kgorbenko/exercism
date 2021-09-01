550e984b5204286d414077db9f13ff9ad0e7265f
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-02 00:17:32 +0300

    Add iteration

--- csharp/log-levels/LogLevels.cs
+++ csharp/log-levels/LogLevels.cs
@@ -1,19 +1,29 @@
 using System;
+using System.Linq;
 
 static class LogLine
 {
-    public static string Message(string logLine)
+    private static readonly string[] availableLogLevels = { "info", "warning", "error" };
+
+    private static (string Level, string Message) Parse(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.Message() method");
+        var level = availableLogLevels.First(x => logLine.Contains($"[{x}]", StringComparison.OrdinalIgnoreCase));
+        var message = logLine.Replace(oldValue: $"[{level}]:", newValue: "", StringComparison.OrdinalIgnoreCase)
+                             .Trim();
+
+        return (Level: level, Message: message);
     }
 
+    public static string Message(string logLine)
+        => Parse(logLine).Message;
+
     public static string LogLevel(string logLine)
-    {
-        throw new NotImplementedException("Please implement the (static) LogLine.LogLevel() method");
-    }
+        => Parse(logLine).Level;
 
     public static string Reformat(string logLine)
     {
-        throw new NotImplementedException("Please implement the (static) LogLine.Reformat() method");
+        var (level, message) = Parse(logLine);
+
+        return $"{message} ({level.ToLower()})";
     }
 }
