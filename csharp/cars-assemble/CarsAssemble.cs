0201ea8bde02df73e2822b8ad6d64749b708b536
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-01 21:57:16 +0300

    Downloaded exercise

---
+++ csharp/cars-assemble/CarsAssemble.cs
@@ -0,0 +1,14 @@
+using System;
+
+static class AssemblyLine
+{
+    public static double ProductionRatePerHour(int speed)
+    {
+        throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
+    }
+
+    public static int WorkingItemsPerMinute(int speed)
+    {
+        throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
+    }
+}
