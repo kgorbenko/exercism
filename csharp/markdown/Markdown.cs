950793c650892bcbe675352227ef307ede048a14
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-01 23:42:58 +0300

    Add iteration

--- csharp/markdown/Markdown.cs
+++ csharp/markdown/Markdown.cs
@@ -1,103 +1,83 @@
 ﻿using System;
-using System.Text.RegularExpressions;
 using System.Linq;
 using System.Collections.Generic;
 
 public class Markdown
 {
-    private static Dictionary<string, string> SimpleTags = new Dictionary<string, string>()
+    private static readonly Dictionary<string, string> simpleTags = new()
     {
-        {"__", "strong"}, {"_", "em" }
+        {"__", "strong"},
+        {"_", "em" }
     };
 
     public static string Parse(string markdown)
     {
-        var lines = markdown.Split("\n");
-        var result = "";
+        var lines = markdown.Split("\n")
+                            .Select(ParseLine)
+                            .ToArray();
 
-        foreach (var line in lines)
-            result += ParseLine(line);
-
-        if (result.Contains("<li>"))
-            result = WrapListItems(result);
-
-        return result;
+        var text = string.Join("", lines);
+        return text.Contains("<li>")
+            ? WrapListItems(text)
+            : text;
     }
 
     private static string ParseLine(string line)
     {
-        line = ParseSimpleTags(line);
-
-        if (line.StartsWith("#"))
-            line = ParseHeader(line);
-
-        else if (line.StartsWith("*"))
-            line = ParseListItem(line);
-
-        else line = $"<p>{line}</p>";
-
-        return line;
+        return ParseSimpleTags(line) switch {
+            var s when s.StartsWith("#") => ParseHeader(s),
+            var s when s.StartsWith("*") => ParseListItem(s),
+            var s => Wrap(text: s, tag: "p")
+        };
     }
 
     private static string ParseHeader(string line)
     {
-        int headerLevel = 0;
+        var headersLength = line.TakeWhile(x => x == '#').Count();
+        var innerHtml = new string(line.Skip(headersLength).ToArray()).TrimStart();
 
-        while (line.StartsWith("#"))
-        {
-            line = line.Remove(0, 1);
-            headerLevel += 1;
-        }
-
-        return Wrap(line.TrimStart(), $"h{headerLevel}");
+        return Wrap(innerHtml, $"h{headersLength}");
     }
 
     private static string ParseListItem(string line)
     {
-        line = line.Remove(0, 1);
+        var innerHtml = new string(line.Skip(count: 1).ToArray()).TrimStart();
 
-        return Wrap(line.TrimStart(), "li");
+        return Wrap(innerHtml, "li");
     }
 
     private static string WrapListItems(string line)
     {
-        var firstOccurrance = line.IndexOf("<li>");
-        var lastOccurrance = line.LastIndexOf("</li>");
+        var firstOccurrence = line.IndexOf("<li>", StringComparison.Ordinal);
+        var lastOccurrence = line.LastIndexOf("</li>", StringComparison.Ordinal);
+        var innerHtml = line.Substring(firstOccurrence, lastOccurrence - firstOccurrence + "</li>".Length);
 
-        var undertag = line.Substring(firstOccurrance, lastOccurrance - firstOccurrance + 5);
-
-        return line.Replace(undertag, $"<ul>{undertag}</ul>");
+        return line.Replace(innerHtml, Wrap(text: innerHtml, tag: "ul"));
     }
 
     private static string ParseSimpleTags(string line)
     {
-        foreach (var pair in SimpleTags)
+        if (!simpleTags.Keys.Any(line.Contains))
         {
-            if (line.Contains(pair.Key))
-            {
-                var undertag = ExtractUndertag(line, pair.Key);
-
-                var wrappedText = Wrap(undertag, pair.Value);
-
-                var textToReplace = $"{pair.Key}{undertag}{pair.Key}";
-
-                line = line.Replace(textToReplace, wrappedText);
-            }
+            return line;
         }
 
-        return line;
+        var (markdownTag, htmlTag) = simpleTags.First(x => line.Contains(x.Key));
+        var innerHtml = ExtractInnerHtml(line, markdownTag);
+        var wrappedHtml = Wrap(innerHtml, htmlTag);
+        var textToReplace = $"{markdownTag}{innerHtml}{markdownTag}";
+
+        return ParseSimpleTags(line.Replace(textToReplace, wrappedHtml));
     }
 
-    private static string ExtractUndertag(string line, string tag)
+    private static string ExtractInnerHtml(string line, string tag)
     {
-        var firstOccurrance = line.IndexOf(tag);
-        var secondOccurrance = line.IndexOf(tag, firstOccurrance + tag.Length);
+        var firstOccurrence = line.IndexOf(tag, StringComparison.Ordinal);
+        var secondOccurrence = line.IndexOf(tag, firstOccurrence + tag.Length, StringComparison.Ordinal);
 
-        return line.Substring(firstOccurrance + tag.Length, secondOccurrance - firstOccurrance - tag.Length);
+        return line.Substring(firstOccurrence + tag.Length, secondOccurrence - firstOccurrence - tag.Length);
     }
 
     private static string Wrap(string text, string tag)
-    {
-        return $"<{tag}>{text}</{tag}>";
-    }
+        => $"<{tag}>{text}</{tag}>";
 }
