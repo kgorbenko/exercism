2baeff450d11e6baae9ec4ac529b105573004764
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-01 22:36:20 +0300

    Add iteration

--- csharp/cars-assemble/CarsAssemble.cs
+++ csharp/cars-assemble/CarsAssemble.cs
@@ -2,13 +2,23 @@ using System;
 
 static class AssemblyLine
 {
-    public static double ProductionRatePerHour(int speed)
-    {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.ProductionRatePerHour() method");
-    }
+    private const int CarsPerHour = 221;
 
-    public static int WorkingItemsPerMinute(int speed)
+    public static double ProductionRatePerHour(int speed) =>
+        CarsPerHour * speed * GetSuccessRate(speed);
+
+    public static int WorkingItemsPerMinute(int speed) =>
+        (int) ProductionRatePerHour(speed) / 60;
+
+    private static double GetSuccessRate(int speed)
     {
-        throw new NotImplementedException("Please implement the (static) AssemblyLine.WorkingItemsPerMinute() method");
+        return speed switch {
+            0             => 0,
+            >= 1 and <= 4 => 1,
+            >= 5 and <= 8 => 0.9,
+            9             => 0.8,
+            10            => 0.77,
+            _             => throw new ArgumentOutOfRangeException(nameof(speed))
+        };
     }
 }
