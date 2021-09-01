c62928ba2427427206e96b05b66ecfe09cf9dd42
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-01 18:23:47 +0300

    Add iteration

--- csharp/triangle/Triangle.cs
+++ csharp/triangle/Triangle.cs
@@ -1,34 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
 public static class Triangle
 {
     public static bool IsScalene(double side1, double side2, double side3)
         => IsTriangle(side1, side2, side3) &&
-            side1 != side2 && side2 != side3 && side1 != side3;
+           side1 != side2 && side2 != side3 && side1 != side3;
 
     public static bool IsIsosceles(double side1, double side2, double side3)
         => IsTriangle(side1, side2, side3) &&
-            (side1 == side2 || side2 == side3 || side1 == side3);
+           (side1 == side2 || side2 == side3 || side1 == side3);
 
 
     public static bool IsEquilateral(double side1, double side2, double side3)
         => IsTriangle(side1, side2, side3) &&
-            side1 == side2 && side1 == side3;
+           side1 == side2 && side1 == side3;
 
     private static bool IsTriangle(double side1, double side2, double side3)
     {
-        return AllSidesHavePositiveLength() && SumOfAnyTwoSidesIsAlwaysMoreThanThirdSide();
-
-        bool AllSidesHavePositiveLength()
+        bool SidesArePositive()
             => side1 > 0 && side2 > 0 && side3 > 0;
 
-        bool SumOfAnyTwoSidesIsAlwaysMoreThanThirdSide()
+        bool SumOfAnyTwoSidesIsMoreThanThird()
             => side1 + side2 > side3 &&
                side1 + side3 > side2 &&
                side2 + side3 > side1;
+
+        return SidesArePositive() && SumOfAnyTwoSidesIsMoreThanThird();
     }
 }
-
-public class TriangleException : Exception { }
