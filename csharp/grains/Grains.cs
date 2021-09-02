3fbe1cd7e098b000937fefb7e5c4069136262523
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-02 22:19:47 +0300

    Add iteration

--- csharp/grains/Grains.cs
+++ csharp/grains/Grains.cs
@@ -1,31 +1,20 @@
 using System;
+using System.Linq;
 
 public static class Grains
 {
     public static ulong Square(int n)
     {
-        ulong onCertainSquare = 1;
-
-        for (int i = 2; i <= n; i++)
+        if (n <= 0 || n > 64)
         {
-            onCertainSquare = onCertainSquare * 2;
+            throw new ArgumentOutOfRangeException(nameof(n));
         }
 
-        return onCertainSquare;
+        return (ulong) Math.Pow(x: 2, n - 1);
     }
 
     public static ulong Total()
-    {
-        ulong sum = 0;
-        ulong onCertainSquare = 1;
-
-        for (int i = 2; i <= 64; i++)
-        {
-            onCertainSquare = onCertainSquare * 2;
-            sum = sum + onCertainSquare;
-        }
-
-        return sum + 1;
-    }
-    
+        => Enumerable.Range(1, 64)
+                     .Select(Square)
+                     .Aggregate((x, y) => x + y);
 }
