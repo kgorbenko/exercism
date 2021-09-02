3a4a4671a6d877333d1e86eaede96d0e69967080
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-02 10:04:59 +0300

    Made pattern matching pretty

--- csharp/cars-assemble/CarsAssemble.cs
+++ csharp/cars-assemble/CarsAssemble.cs
@@ -13,12 +13,12 @@ static class AssemblyLine
     private static double GetSuccessRate(int speed)
     {
         return speed switch {
-            0             => 0,
-            >= 1 and <= 4 => 1,
-            >= 5 and <= 8 => 0.9,
-            9             => 0.8,
-            10            => 0.77,
-            _             => throw new ArgumentOutOfRangeException(nameof(speed))
+            0    => 0,
+            <= 4 => 1,
+            <= 8 => 0.9,
+            9    => 0.8,
+            10   => 0.77,
+            _    => throw new ArgumentOutOfRangeException(nameof(speed))
         };
     }
 }
