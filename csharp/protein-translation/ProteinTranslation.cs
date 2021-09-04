eb6804bfc51c255a52a6fe09ffa3309619729c24
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-04 18:44:42 +0300

    Add iteration

--- csharp/protein-translation/ProteinTranslation.cs
+++ csharp/protein-translation/ProteinTranslation.cs
@@ -1,35 +1,41 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 public static class ProteinTranslation
 {
-    private static readonly Dictionary<string, string> codonToProteinMap = new() {
-        ["AUG"] = "Methionine",
-        ["UUU"] = "Phenylalanine",
-        ["UUC"] = "Phenylalanine",
-        ["UUA"] = "Leucine",
-        ["UUG"] = "Leucine",
-        ["UCU"] = "Serine",
-        ["UCC"] = "Serine",
-        ["UCA"] = "Serine",
-        ["UCG"] = "Serine",
-        ["UAU"] = "Tyrosine",
-        ["UAC"] = "Tyrosine",
-        ["UGU"] = "Cysteine",
-        ["UGC"] = "Cysteine",
-        ["UGG"] = "Tryptophan"
-    };
-
     private static readonly HashSet<string> terminatingCodons = new(new[] {
         "UAA",
         "UAG",
         "UGA"
     });
 
+    private static string ToProtein(string strand)
+        => strand switch {
+            "AUG" => "Methionine",
+            "UUU" => "Phenylalanine",
+            "UUC" => "Phenylalanine",
+            "UUA" => "Leucine",
+            "UUG" => "Leucine",
+            "UCU" => "Serine",
+            "UCC" => "Serine",
+            "UCA" => "Serine",
+            "UCG" => "Serine",
+            "UAU" => "Tyrosine",
+            "UAC" => "Tyrosine",
+            "UGU" => "Cysteine",
+            "UGC" => "Cysteine",
+            "UGG" => "Tryptophan",
+            _ => throw new ArgumentException("Invalid strand.")
+        };
+
+    private static IEnumerable<string> GetCodons(string strand, int codonSize)
+        => Enumerable.Range(start: 0, count: strand.Length / codonSize)
+                     .Select(x => strand[(x * codonSize) .. (x * codonSize + codonSize)]);
+
     public static string[] Proteins(string strand)
-        => Enumerable.Range(start: 0, count: strand.Length / 3)
-                     .Select(x => strand.Substring(startIndex: x * 3, length: 3))
-                     .TakeWhile(x => !terminatingCodons.Contains(x))
-                     .Select(x => codonToProteinMap[x])
-                     .ToArray();
+        => GetCodons(strand, codonSize: 3)
+           .TakeWhile(x => !terminatingCodons.Contains(x))
+           .Select(ToProtein)
+           .ToArray();
 }
