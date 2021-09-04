1f53329283d898ac97028c3c3e03ceb86804df8c
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-04 17:36:12 +0300

    Add iteration

--- csharp/strain/Strain.cs
+++ csharp/strain/Strain.cs
@@ -1,18 +1,27 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 public static class Strain
 {
     public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
     {
         foreach (var item in collection)
-            if (predicate(item)) yield return item;
+        {
+            if (predicate(item))
+            {
+                yield return item;
+            }
+        }
     }
 
     public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
     {
         foreach (var item in collection)
-            if (!predicate(item)) yield return item;
+        {
+            if (!predicate(item))
+            {
+                yield return item;
+            }
+        }
     }
 }
