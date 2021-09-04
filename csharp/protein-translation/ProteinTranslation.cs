9ba9f21446723735c3371a8ab7a8dc42514fa7f9
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-04 18:25:41 +0300

    Add iteration

--- csharp/protein-translation/ProteinTranslation.cs
+++ csharp/protein-translation/ProteinTranslation.cs
@@ -1,47 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 public static class ProteinTranslation
 {
-    private static readonly Dictionary<string, string[]> map = new Dictionary<string, string[]>()
-    {
-        {"Methionine", new string[]{"AUG"} },
-        {"Phenylalanine", new string[]{"UUU", "UUC"} },
-        {"Leucine", new string[]{"UUA", "UUG"} },
-        {"Serine", new string[]{"UCU", "UCC", "UCA", "UCG"} },
-        {"Tyrosine", new string[]{"UAU", "UAC"} },
-        {"Cysteine", new string[]{"UGU", "UGC"} },
-        {"Tryptophan", new string[]{"UGG"}},
-        {"STOP", new string[]{"UAA", "UAG", "UGA" } }
+    private static readonly Dictionary<string, string> codonToProteinMap = new() {
+        ["AUG"] = "Methionine",
+        ["UUU"] = "Phenylalanine",
+        ["UUC"] = "Phenylalanine",
+        ["UUA"] = "Leucine",
+        ["UUG"] = "Leucine",
+        ["UCU"] = "Serine",
+        ["UCC"] = "Serine",
+        ["UCA"] = "Serine",
+        ["UCG"] = "Serine",
+        ["UAU"] = "Tyrosine",
+        ["UAC"] = "Tyrosine",
+        ["UGU"] = "Cysteine",
+        ["UGC"] = "Cysteine",
+        ["UGG"] = "Tryptophan"
     };
 
+    private static readonly HashSet<string> terminatingCodons = new(new[] {
+        "UAA",
+        "UAG",
+        "UGA"
+    });
+
     public static string[] Proteins(string strand)
-    {
-        var codons = DivideStrandToCodons(strand).ToArray();
-        var proteins = new List<string>();
-
-        foreach (var codon in codons)
-        {
-            if (map["STOP"].Contains(codon)) return proteins.ToArray();
-
-            foreach (var pair in map)
-            {
-                if (pair.Value.Contains(codon)) proteins.Add(pair.Key);
-            }
-        }
-
-        return proteins.ToArray();
-    }
-
-    private static IEnumerable<string> DivideStrandToCodons(string strand)
-    {
-        var startIndex = 0;
-
-        for (int i = 0; i < strand.Length / 3; i++)
-        {
-            yield return strand.Substring(startIndex, 3);
-            startIndex += 3;
-        }
-    }
+        => Enumerable.Range(start: 0, count: strand.Length / 3)
+                     .Select(x => strand.Substring(startIndex: x * 3, length: 3))
+                     .TakeWhile(x => !terminatingCodons.Contains(x))
+                     .Select(x => codonToProteinMap[x])
+                     .ToArray();
 }
