84ce39f0ae8df931bf91fd73e3a945601202b4d6
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-04 13:56:23 +0300

    Add current solution

---
+++ csharp/proverb/Proverb.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Proverb
+{
+    public static string[] Recite(string[] subjects)
+    {
+        var proverb = new string[subjects.Length];
+
+        if (subjects.Length == 0) return proverb;
+        if (subjects.Length == 1)
+        {
+            proverb[0] = $"And all for the want of a {subjects[0]}.";
+            return proverb;
+        }
+
+        for (int i = 1; i < subjects.Length; i++)
+            proverb[i-1] = $"For want of a {subjects[i-1]} the {subjects[i]} was lost.";
+
+        proverb[subjects.Length - 1] = $"And all for the want of a {subjects[0]}.";
+
+        return proverb;
+    }
+}
