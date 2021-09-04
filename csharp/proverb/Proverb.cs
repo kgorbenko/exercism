86e90124e82b0285bcbe275315eb18f2131fd9a3
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-04 17:30:37 +0300

    Add iteration

--- csharp/proverb/Proverb.cs
+++ csharp/proverb/Proverb.cs
@@ -1,25 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 public static class Proverb
 {
     public static string[] Recite(string[] subjects)
-    {
-        var proverb = new string[subjects.Length];
-
-        if (subjects.Length == 0) return proverb;
-        if (subjects.Length == 1)
-        {
-            proverb[0] = $"And all for the want of a {subjects[0]}.";
-            return proverb;
-        }
-
-        for (int i = 1; i < subjects.Length; i++)
-            proverb[i-1] = $"For want of a {subjects[i-1]} the {subjects[i]} was lost.";
-
-        proverb[subjects.Length - 1] = $"And all for the want of a {subjects[0]}.";
-
-        return proverb;
-    }
+        => subjects.Length == 0
+            ? Array.Empty<string>()
+            : Enumerable.Range(start: 1, subjects.Length - 1)
+                        .Select(i => $"For want of a {subjects[i - 1]} the {subjects[i]} was lost.")
+                        .Append($"And all for the want of a {subjects[0]}.")
+                        .ToArray();
 }
