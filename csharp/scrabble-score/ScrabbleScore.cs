38922f73f137a9950b6dcfa85da518c709f8a1c3
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-04 13:52:18 +0300

    Add iteration

--- csharp/scrabble-score/ScrabbleScore.cs
+++ csharp/scrabble-score/ScrabbleScore.cs
@@ -1,30 +1,19 @@
-using System;
 using System.Linq;
-using System.Collections.Generic;
 
 public static class ScrabbleScore
 {
-    private static Dictionary<int, char[]> map = new Dictionary<int, char[]>()
-    {
-        {1, new char[] {'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' } },
-        {2, new char[] {'D', 'G'} },
-        {3, new char[] {'B', 'C', 'M', 'P'} },
-        {4, new char[] {'F', 'H', 'V', 'W', 'Y'} },
-        {5, new char[] {'K'} },
-        {8, new char[] {'J', 'X'} },
-        {10, new char[] {'Q', 'Z'} }
+    private static readonly (int Points, char[] Letters)[] pointsNumberToLetters = {
+        (1, new[] { 'A', 'E', 'I', 'O', 'U', 'L', 'N', 'R', 'S', 'T' }),
+        (2, new[] { 'D', 'G' }),
+        (3, new[] { 'B', 'C', 'M', 'P' }),
+        (4, new[] { 'F', 'H', 'V', 'W', 'Y' }),
+        (5, new[] { 'K' }),
+        (8, new[] { 'J', 'X' }),
+        (10, new[] { 'Q', 'Z' })
     };
 
     public static int Score(string input)
-    {
-        var score = 0;
-
-        foreach (var letter in input.ToUpper())
-        {
-            foreach (var pair in map)
-                if (pair.Value.Contains(letter)) score += pair.Key;
-        }
-
-        return score;
-    }
+        => pointsNumberToLetters.Sum(pointsToLetters
+            => input.Count(letter => pointsToLetters.Letters.Contains(char.ToUpper(letter)))
+            * pointsToLetters.Points);
 }
