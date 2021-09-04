2483048c7e4ad0f9a09e869e75ababf623715ca4
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-04 18:48:27 +0300

    One more iteration

--- csharp/protein-translation/ProteinTranslation.cs
+++ csharp/protein-translation/ProteinTranslation.cs
@@ -1,33 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 public static class ProteinTranslation
 {
+    private static readonly Dictionary<string, string> codonToProteinMap = new() {
+        ["AUG"] = "Methionine",
+        ["UUU"] = "Phenylalanine",
+        ["UUC"] = "Phenylalanine",
+        ["UUA"] = "Leucine",
+        ["UUG"] = "Leucine",
+        ["UCU"] = "Serine",
+        ["UCC"] = "Serine",
+        ["UCA"] = "Serine",
+        ["UCG"] = "Serine",
+        ["UAU"] = "Tyrosine",
+        ["UAC"] = "Tyrosine",
+        ["UGU"] = "Cysteine",
+        ["UGC"] = "Cysteine",
+        ["UGG"] = "Tryptophan"
+    };
+
     private static readonly HashSet<string> terminatingCodons = new(new[] {
         "UAA",
         "UAG",
         "UGA"
     });
-
-    private static string ToProtein(string strand)
-        => strand switch {
-            "AUG" => "Methionine",
-            "UUU" => "Phenylalanine",
-            "UUC" => "Phenylalanine",
-            "UUA" => "Leucine",
-            "UUG" => "Leucine",
-            "UCU" => "Serine",
-            "UCC" => "Serine",
-            "UCA" => "Serine",
-            "UCG" => "Serine",
-            "UAU" => "Tyrosine",
-            "UAC" => "Tyrosine",
-            "UGU" => "Cysteine",
-            "UGC" => "Cysteine",
-            "UGG" => "Tryptophan",
-            _ => throw new ArgumentException("Invalid strand.")
-        };
 
     private static IEnumerable<string> GetCodons(string strand, int codonSize)
         => Enumerable.Range(start: 0, count: strand.Length / codonSize)
@@ -36,6 +33,6 @@ public static class ProteinTranslation
     public static string[] Proteins(string strand)
         => GetCodons(strand, codonSize: 3)
            .TakeWhile(x => !terminatingCodons.Contains(x))
-           .Select(ToProtein)
+           .Select(x => codonToProteinMap[x])
            .ToArray();
 }
