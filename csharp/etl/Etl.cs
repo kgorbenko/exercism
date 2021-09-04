a628cd07f360f8158be670362653b63f30a938a8
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-04 19:08:38 +0300

    Add iteration

--- csharp/etl/Etl.cs
+++ csharp/etl/Etl.cs
@@ -4,6 +4,6 @@ using System.Linq;
 public static class Etl
 {
     public static Dictionary<string, int> Transform(Dictionary<int, string[]> old)
-        => old.SelectMany(pair => pair.Value.Select(y => (Key: y, Value: pair.Key)))
+        => old.SelectMany(pair => pair.Value.Select(s => (Key: s, Value: pair.Key)))
               .ToDictionary(x => x.Key.ToLower(), x => x.Value);
 }
