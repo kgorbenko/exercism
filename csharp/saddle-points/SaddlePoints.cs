eabb38d908fa4ae070d9d8cda1c1a9e3c9bf42a8
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-10 08:57:38 +0300

    Add iteration

--- csharp/saddle-points/SaddlePoints.cs
+++ csharp/saddle-points/SaddlePoints.cs
@@ -1,40 +1,30 @@
-using System;
 using System.Collections.Generic;
+using System.Linq;
 
-public class SaddlePoints
+public static class SaddlePoints
 {
-    private int[,] matrix;
-
-    public SaddlePoints(int[,] values)
+    public static IEnumerable<(int, int)> Calculate(int[,] matrix)
     {
-        matrix = values;
+        return Enumerable.Range(0, matrix.GetLength(0))
+                         .SelectMany(x => Enumerable.Range(0, matrix.GetLength(1)).Select(y => (x, y)))
+                         .Where(x => IsSaddle(x, matrix))
+                         .Select(x => (x.x + 1, x.y + 1));
     }
 
-    public IEnumerable<(int, int)> Calculate()
+    private static bool IsSaddle((int x, int y) point, int[,] matrix)
     {
-        var points = new List<(int, int)>();
+        var value = matrix[point.x, point.y];
+        var isLeastInColumn = GetColumn(point.y, matrix).All(x => x >= value);
+        var isLargestInRow = GetRow(point.x, matrix).All(x => x <= value);
 
-        for (int i = 0; i < matrix.GetLength(0); i++)
-            for (int j = 0; j < matrix.GetLength(1); j++)
-            {
-                if (IsSaddle((i, j))) points.Add((i, j));
-            }
-
-        return points;
+        return isLeastInColumn && isLargestInRow;
     }
 
-    private bool IsSaddle((int, int) point)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            if (matrix[point.Item1, j] > matrix[point.Item1, point.Item2]) return false; 
-        }
+    private static IEnumerable<int> GetRow(int rowNumber, int[,] matrix)
+        => Enumerable.Range(0, matrix.GetLength(1))
+                     .Select(y => matrix[rowNumber, y]);
 
-        for (int i = 0; i < matrix.GetLength(0); i++)
-        {
-            if (matrix[i, point.Item2] < matrix[point.Item1, point.Item2]) return false;
-        }
-
-        return true;
-    }
+    private static IEnumerable<int> GetColumn(int columnNumber, int[,] matrix)
+        => Enumerable.Range(0, matrix.GetLength(0))
+                     .Select(x => matrix[x, columnNumber]);
 }
