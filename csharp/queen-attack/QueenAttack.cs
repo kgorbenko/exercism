febdac59d2659528faed1784730aedb159f4b6d2
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-10 18:14:23 +0300

    Add iteration

--- csharp/queen-attack/QueenAttack.cs
+++ csharp/queen-attack/QueenAttack.cs
@@ -1,30 +1,37 @@
 ﻿using System;
+
 public class Queen
 {
     public Queen(int row, int column)
     {
-        if (row < 0 || row >= 8 || column < 0 || column >= 8)
-            throw new ArgumentOutOfRangeException("Queen should be placed on board");
+        if (row is < 0 or >= 8)
+        {
+            throw new ArgumentOutOfRangeException(nameof(row));
+        }
+        if (column is < 0 or >= 8)
+        {
+            throw new ArgumentOutOfRangeException(nameof(column));
+        }
+
         Row    = row;
         Column = column;
     }
+
     public int Row { get; }
     public int Column { get; }
 }
+
 public static class QueenAttack
 {
     public static bool CanAttack(Queen white, Queen black)
     {
-        return white.Row == black.Row || white.Column == black.Column || OnDiagonal();
-        bool OnDiagonal()
-        {
-            var rowDifference = Math.Abs(white.Row - black.Row);
-            var columnDifference = Math.Abs(white.Column - black.Column);
-            return rowDifference == columnDifference;
-        }
+        var onSameRow = white.Row == black.Row;
+        var onSameColumn = white.Column == black.Column;
+        var onDiagonal = Math.Abs(white.Row - black.Row) ==
+                         Math.Abs(white.Column - black.Column);
+
+        return onSameRow || onSameColumn || onDiagonal;
     }
-    public static Queen Create(int row, int column)
-    {
-        return new Queen(row, column);
-    }
+
+    public static Queen Create(int row, int column) => new(row, column);
 }
