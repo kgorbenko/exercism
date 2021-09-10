e38f14b8f99e0e7070320bda140d42a4701200d7
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-10 17:43:05 +0300

    Add iteration

--- csharp/saddle-points/SaddlePoints.cs
+++ csharp/saddle-points/SaddlePoints.cs
@@ -3,28 +3,29 @@ using System.Linq;
 
 public static class SaddlePoints
 {
+    private static int GetRowCount(int[,] matrix) => matrix.GetLength(0);
+    private static int GetColumnCount(int[,] matrix) => matrix.GetLength(1);
+
+    private static IEnumerable<IEnumerable<int>> GetRows(int[,] matrix)
+        => Enumerable.Range(0, GetRowCount(matrix))
+                     .Select(_ => Enumerable.Range(0, GetColumnCount(matrix)))
+                     .Select((x, i) => x.Select(j => matrix[i, j]));
+
+    private static IEnumerable<IEnumerable<int>> GetColumns(int[,] matrix)
+        => Enumerable.Range(0, GetColumnCount(matrix))
+                     .Select(_ => Enumerable.Range(0, GetRowCount(matrix)))
+                     .Select((x, i) => x.Select(j => matrix[j, i]));
+
+    private static IEnumerable<(int i, int j)> GetValues(int[,] matrix)
+        => Enumerable.Range(0, GetRowCount(matrix))
+                     .SelectMany(x => Enumerable.Range(0, GetColumnCount(matrix)).Select(y => (x, y)));
+
     public static IEnumerable<(int, int)> Calculate(int[,] matrix)
     {
-        return Enumerable.Range(0, matrix.GetLength(0))
-                         .SelectMany(x => Enumerable.Range(0, matrix.GetLength(1)).Select(y => (x, y)))
-                         .Where(x => IsSaddle(x, matrix))
-                         .Select(x => (x.x + 1, x.y + 1));
+        var maxValuesInRows = GetRows(matrix).Select(x => x.Max()).ToArray();
+        var minValuesInColumns = GetColumns(matrix).Select(x => x.Min()).ToArray();
+
+        return GetValues(matrix).Where(x => maxValuesInRows[x.i] == minValuesInColumns[x.j])
+                                .Select(x => (x.i + 1, x.j + 1));
     }
-
-    private static bool IsSaddle((int x, int y) point, int[,] matrix)
-    {
-        var value = matrix[point.x, point.y];
-        var isLeastInColumn = GetColumn(point.y, matrix).All(x => x >= value);
-        var isLargestInRow = GetRow(point.x, matrix).All(x => x <= value);
-
-        return isLeastInColumn && isLargestInRow;
-    }
-
-    private static IEnumerable<int> GetRow(int rowNumber, int[,] matrix)
-        => Enumerable.Range(0, matrix.GetLength(1))
-                     .Select(y => matrix[rowNumber, y]);
-
-    private static IEnumerable<int> GetColumn(int columnNumber, int[,] matrix)
-        => Enumerable.Range(0, matrix.GetLength(0))
-                     .Select(x => matrix[x, columnNumber]);
 }
