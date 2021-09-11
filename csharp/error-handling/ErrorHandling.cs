73291389b87971b733eb631d40bc305840ca1941
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-11 13:05:58 +0300

    Add current solution

---
+++ csharp/error-handling/ErrorHandling.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+public static class ErrorHandling
+{
+    public static void HandleErrorByThrowingException()
+    {
+        throw new Exception();
+    }
+
+    public static int? HandleErrorByReturningNullableType(string input)
+    {
+        try
+        {
+            return int.Parse(input);
+        }
+        catch
+        {
+            return null;
+        }
+    }
+
+    public static bool HandleErrorWithOutParam(string input, out int result)
+    {
+        try
+        {
+            result = int.Parse(input);
+            return true;
+        }
+        catch
+        {
+            result = 0;
+            return false;
+        }
+    }
+
+    public static void DisposableResourcesAreDisposedWhenExceptionIsThrown(IDisposable disposableObject)
+    {
+        try
+        {
+            throw new Exception();
+        }
+        catch(Exception e)
+        {
+            disposableObject.Dispose();
+            throw e;
+        }
+    }
+}
