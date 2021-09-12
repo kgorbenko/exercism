7a6b1444a07baad3db59c3161523e95cb97d4fce
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-12 22:42:54 +0300

    Added iteration

--- csharp/matching-brackets/MatchingBrackets.cs
+++ csharp/matching-brackets/MatchingBrackets.cs
@@ -1,9 +1,47 @@
-using System;
+using System.Collections.Generic;
+using System.Linq;
 
 public static class MatchingBrackets
 {
+    private static readonly (char Opening, char Closing)[] brackets = {
+        ('(', ')'),
+        ('{', '}'),
+        ('[', ']')
+    };
+
+    private static char GetOpeningBracket(char closingBracket)
+        => brackets.First(x => x.Closing == closingBracket).Opening;
+
+    private static IEnumerable<char> GetOpeningBrackets()
+        => brackets.Select(x => x.Opening);
+
+    private static bool IsOpening(this char bracket)
+        => GetOpeningBrackets().Contains(bracket);
+
+    private static IEnumerable<char> GetClosingBrackets()
+        => brackets.Select(x => x.Closing).ToArray();
+
     public static bool IsPaired(string input)
     {
-        throw new NotImplementedException();
+        var bracketsStack = new Stack<char>();
+        var allBrackets = GetOpeningBrackets().Concat(GetClosingBrackets());
+
+        foreach (var character in input.Where(x => allBrackets.Contains(x)))
+        {
+            if (!character.IsOpening())
+            {
+                if (!bracketsStack.TryPeek(out var opening) || GetOpeningBracket(character) != opening)
+                {
+                    return false;
+                }
+
+                bracketsStack.Pop();
+            } else
+            {
+                bracketsStack.Push(character);
+            }
+        }
+
+        return !bracketsStack.Any();
     }
 }
