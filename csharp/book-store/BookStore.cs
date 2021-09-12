aeab60343066ca46cdeecafbbc42ef129c7a0667
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-12 16:45:54 +0300

    Add iteration

--- csharp/book-store/BookStore.cs
+++ csharp/book-store/BookStore.cs
@@ -1,44 +1,62 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 public static class BookStore
 {
+    private const int SeriesLength = 5;
     private const decimal BookPrice = 8m;
 
-    private static readonly Dictionary<int, decimal> discounts = new() {
-        { 1, 1.0m },
-        { 2, 0.95m },
-        { 3, 0.90m },
-        { 4, 0.80m },
-        { 5, 0.75m }
-    };
-
     public static decimal Total(IEnumerable<int> books)
-        => books.Any()
-            ? GetGroupLengths(books.ToArray()).Select(GetGroupPrice).Min()
-            : 0;
-
-    private static IEnumerable<int[]> GetGroupLengths(IEnumerable<int> books)
-        => Enumerable.Range(1, discounts.Count)
-                     .Select(maxGroupCount => GetGroups(books.SortByCount(), maxGroupCount)
-                                              .Select(x => x.Length)
-                                              .ToArray());
-
-    private static IEnumerable<int[]> GetGroups(IEnumerable<int> books, int maxGroupCount)
     {
-        if (!books.Any())
+        var booksArray = books.ToArray();
+
+        return booksArray.Any()
+            ? GetGroupLengths(booksArray).Select(GetGroupPrice).Min()
+            : 0;
+    }
+
+    private static decimal GetGroupPrice(IEnumerable<int> groupsLengths)
+        => groupsLengths.Sum(length => BookPrice * length * GetDiscount(length));
+
+    private static decimal GetDiscount(int booksCount)
+        => booksCount switch {
+            1 => 1.0m,
+            2 => 0.95m,
+            3 => 0.90m,
+            4 => 0.80m,
+            5 => 0.75m,
+            _ => throw new InvalidOperationException("Invalid number of books. Cannot count discount.")
+        };
+
+    private static IEnumerable<IEnumerable<int>> GetGroupLengths(int[] books)
+        => Enumerable.Range(start: 1, count: SeriesLength)
+                     .Select(maxGroupCount => books.SortByCount()
+                                                   .GetGroups(maxGroupCount)
+                                                   .Select(x => x.Length));
+
+    private static IEnumerable<int> SortByCount(this IEnumerable<int> books)
+        => books.GroupBy(x => x)
+                .OrderByDescending(x => x.Count())
+                .SelectMany(x => x);
+
+    private static IEnumerable<int[]> GetGroups(this IEnumerable<int> books, int maxGroupCount)
+    {
+        var booksArray = books.ToArray();
+
+        if (!booksArray.Any())
         {
             return Enumerable.Empty<int[]>();
         }
 
-        var group = GetNextGroup(books, maxGroupCount).ToArray();
+        var group = GetNextGroup(booksArray, maxGroupCount).ToArray();
 
-        return new[] { group }.Concat(GetGroups(books.Remove(group), maxGroupCount));
+        return new[] { group }.Concat(GetGroups(booksArray.RemoveOnce(group), maxGroupCount));
     }
 
-    private static IEnumerable<int> Remove(this IEnumerable<int> books, IEnumerable<int> toRemove)
+    private static IEnumerable<int> RemoveOnce(this IEnumerable<int> books, IEnumerable<int> toRemove)
     {
-        var list = books.ToList();
+        var list = new List<int>(books);
 
         foreach (var itemToRemove in toRemove)
         {
@@ -48,17 +66,9 @@ public static class BookStore
         return list;
     }
 
-    private static IEnumerable<int> SortByCount(this IEnumerable<int> books)
-        => books.GroupBy(x => x)
-                .OrderByDescending(x => x.Count())
-                .SelectMany(x => x);
-
     private static IEnumerable<int> GetNextGroup(IEnumerable<int> books, int maxGroupCount)
         => books.GroupBy(x => x)
                 .Where(group => group.Any())
                 .Take(maxGroupCount)
                 .Select(x => x.Key);
-
-    private static decimal GetGroupPrice(IEnumerable<int> groupsLengths)
-        => groupsLengths.Sum(length => BookPrice * length * discounts[length]);
 }
