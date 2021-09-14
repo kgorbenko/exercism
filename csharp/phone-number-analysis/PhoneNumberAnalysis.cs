8e9ee0c58176ee69475ffe82b4b37c67351cd8f7
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-14 10:56:08 +0300

    Add iteration

--- csharp/phone-number-analysis/PhoneNumberAnalysis.cs
+++ csharp/phone-number-analysis/PhoneNumberAnalysis.cs
@@ -1,14 +1,12 @@
-using System;
-
 public static class PhoneNumber
 {
     public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.Analyze() method");
+        var parts = phoneNumber.Split("-");
+
+        return (parts[0] == "212", parts[1] == "555", parts[2]);
     }
 
     public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
-    {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.IsFake() method");
-    }
+        => phoneNumberInfo.IsFake;
 }
