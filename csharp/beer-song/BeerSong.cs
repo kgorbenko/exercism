58f9b03f2427d559528fa2f1acea48875ccd4e86
Kirill Gorbenko <kirill.gorbenko00@gmail.com>
2021-09-14 09:42:38 +0300

    Add iteration

--- csharp/beer-song/BeerSong.cs
+++ csharp/beer-song/BeerSong.cs
@@ -1,43 +1,26 @@
-﻿using System.Text;
+﻿using System;
+using System.Linq;
 
 public static class BeerSong
 {
     public static string Recite(int startBottles, int takeDown)
+        => string.Join("\n\n", Enumerable.Range(startBottles - takeDown + 1, takeDown)
+                                         .Select(GetVerse)
+                                         .Reverse()
+                                         .ToArray());
+
+    private static string GetVerse(int bottles)
     {
-        var song = new StringBuilder();
-        int bottles = startBottles;
-
-        for (int i = 0; i < takeDown; i++)
-        {
-            var firstLine = GetFirstLine(bottles);
-            var secondLine = GetSecondLine(bottles);
-
-            bottles -= 1;
-            song.Append($"{firstLine}\n{secondLine}\n\n");
-        }
-            
-        return song.Remove(song.Length - 2, 2).ToString();
+        return bottles switch {
+            var n and > 2 => $"{n} bottles of beer on the wall, {n} bottles of beer.\n" +
+                                $"Take one down and pass it around, {n - 1} bottles of beer on the wall.",
+            2 => "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+                 "Take one down and pass it around, 1 bottle of beer on the wall.",
+            1 => "1 bottle of beer on the wall, 1 bottle of beer.\n" +
+                 "Take it down and pass it around, no more bottles of beer on the wall.",
+            0 => "No more bottles of beer on the wall, no more bottles of beer.\n" +
+                 "Go to the store and buy some more, 99 bottles of beer on the wall.",
+            _ => throw new ArgumentOutOfRangeException(nameof(bottles))
+        };
     }
-
-    private static string GetFirstLine(int bottles)
-    {
-        var numberOfBottles = $"{(bottles != 0 ? bottles.ToString() : "No more")}";
-        var bottleWord = GetSingleOrPluralBottleForm(bottles);
-
-        return $"{numberOfBottles} {bottleWord} of beer on the wall, {numberOfBottles.ToLower()} {bottleWord} of beer.";
-    }
-
-    private static string GetSecondLine(int bottles)
-    {
-        if (bottles == 0) return "Go to the store and buy some more, 99 bottles of beer on the wall.";
-
-        var numberOfBottles = $"{(bottles == 1 ? "it" : "one")}";
-        var bottlesLeft = $"{(bottles - 1 > 0 ? (bottles - 1).ToString() : "no more")}";
-        var bottleWord = GetSingleOrPluralBottleForm(bottles - 1);
-
-        return $"Take {numberOfBottles} down and pass it around, {bottlesLeft} {bottleWord} of beer on the wall.";
-    }
-
-    private static string GetSingleOrPluralBottleForm(int bottles)
-        => $"bottle{(bottles == 1 ? string.Empty : "s")}";
 }
