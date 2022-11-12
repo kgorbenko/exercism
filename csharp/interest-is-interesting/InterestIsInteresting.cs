af6ba10ec4a1a7a2606efe5a734b31e6fdb01df9
Kirill Gorbenko <kgorbenkodev@proton.me>
2022-11-12 16:19:55 +0300

    Upgrade `interest-is-interesting` exercise

--- csharp/interest-is-interesting/InterestIsInteresting.cs
+++ csharp/interest-is-interesting/InterestIsInteresting.cs
@@ -1,27 +1,24 @@
-using System;
-
-static class SavingsAccount
+public static class SavingsAccount
 {
-    public static float InterestRate(decimal balance)
-    {
-        return balance switch {
-            < 0     => -3.213f,
+    public static float InterestRate(decimal balance) =>
+        balance switch {
+            < 0     => 3.213f,
             < 1000  => 0.5f,
             < 5000  => 1.621f,
             >= 5000 => 2.475f
         };
-    }
+
+    public static decimal Interest(decimal balance) =>
+        (decimal)InterestRate(balance) * balance / 100;
 
     public static decimal AnnualBalanceUpdate(decimal balance)
-        => balance + balance * Math.Sign(balance) * (decimal)InterestRate(balance) / 100;
+        => balance + Interest(balance);
 
     public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
         => CalculateYears(balance, targetBalance, years: 0);
 
-    private static int CalculateYears(decimal balance, decimal targetBalance, int years)
-    {
-        return balance >= targetBalance
+    private static int CalculateYears(decimal balance, decimal targetBalance, int years) =>
+        balance >= targetBalance
             ? years
-            : CalculateYears(AnnualBalanceUpdate(balance) , targetBalance, years + 1);
-    }
+            : CalculateYears(AnnualBalanceUpdate(balance), targetBalance, years + 1);
 }
