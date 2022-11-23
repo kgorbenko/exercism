17e9b7ab0c66a2361f725ea90f0894c7b40ef398
Kirill Gorbenko <kgorbenkodev@proton.me>
2022-11-23 07:50:34 +0300

    Upgrade `pangram` exercise

--- csharp/pangram/Pangram.cs
+++ csharp/pangram/Pangram.cs
@@ -1,11 +1,11 @@
+using System;
 using System.Linq;
 
 public static class Pangram
 {
     public static bool IsPangram(string input)
     {
-        var upperCase = input.ToUpper();
-
+        var upperCase = input.ToUpperInvariant();
         return "ABCDEFGHIJKLMNOPQRSTUVWXYZ".All(index => upperCase.Contains(index));
     }
 }
